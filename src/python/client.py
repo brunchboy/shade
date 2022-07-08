c7f39dbecd286cf65ad794addd6e026cabfeff1d
James Elliott <james@deepsymmetry.org>
2022-07-08 01:07:13 -0500

    Fix default web socket URL.

--- src/python/client.py
+++ src/python/client.py
@@ -90,7 +90,9 @@ if __name__ == "__main__":
     websocket.enableTrace(True)
     token = keyring.get_password("shade.deepsymmetry.org", "x-shade-token")
     # For local development/testing, set SHADE_WS_URL to "ws://localhost:3000/ws"
-    ws = websocket.WebSocketApp(os.environ.get("SHADE_WS_URL", "wss://shade.deepsymmery.org/ws"),
+    ws_url = os.environ.get("SHADE_WS_URL", "wss://shade.deepsymmetry.org/ws")
+    print("Opening web socket to " + ws_url)
+    ws = websocket.WebSocketApp(ws_url,
                                 on_open=on_open,
                                 on_message=on_message,
                                 on_error=on_error,
