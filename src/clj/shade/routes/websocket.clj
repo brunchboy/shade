265110e3c74386cea2b99553a918d35ad9286fad
James Elliott <james@deepsymmetry.org>
2023-05-23 12:19:17 -0500

    Properly capture pre-sunblock shade state.

--- src/clj/shade/routes/websocket.clj
+++ src/clj/shade/routes/websocket.clj
@@ -464,7 +464,7 @@
   (when (seq unobstructed)
     ;; Save the starting positions of unobstructed shades so we can restore them when sunblock ends.
     (doseq [shade unobstructed]
-      (let [level (get-in state [(:id shade) :level])]
+      (let [level (get-in state [:shades (:id shade) :level])]
         (log/info "Saving sunblock_restore level of shade" (:name shade) "as" level)
         (db/set-shade-sunblock-restore! {:id               (:id shade)
                                          :sunblock_restore level})))
